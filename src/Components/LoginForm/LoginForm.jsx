088a43189a49060e36f22c2a23334b88c0f86e79
coparkBrandon <coparkcreative@gmail.com>
2024-02-09 16:20:43 -0600

    initial commit - completed project

---
+++ src/Components/LoginForm/LoginForm.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import './LoginForm.css'
+import { FaUser, FaLock } from "react-icons/fa";
+
+const LoginForm = () => {
+  return (
+    <div>
+      <div className="wrapper">
+        <form>
+            <h1>Login</h1>
+            <div className="input-box">
+                <input type='text' placeholder='username' required />
+                <FaUser className='icon' />
+            </div>
+            <div className="input-box">
+                <input type='password' placeholder='password' required />
+                <FaLock className='icon' />
+            </div>
+            <div className="remember-forgot">
+                <label><input type='checkbox'></input>Remember Me</label>
+                <a href='#'>Forgot Password?</a>
+            </div>
+
+            <button type='submit'>Login</button>
+
+            <div className="register-link">
+                <p>Don't have an account? <a href='#'>Register</a></p>
+            </div>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default LoginForm
